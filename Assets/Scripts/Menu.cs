ddf2de94d05102b1eb48db00c019b8d282b9f13b
Juan Pablo <jcamacho249@unab.edu.co>
2020-11-03 01:04:46 -0500

    Tutorial Main Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -16,7 +16,7 @@ public class Menu : MonoBehaviour
 
     public Text title;
     public Text Creditos;
-
+    public Image TutorialImage;
 
     public Vector3 IniPosOp1;
     public Vector3 IniPosOp2;
@@ -37,9 +37,12 @@ public class Menu : MonoBehaviour
         Opcion3 = GameObject.Find("Opcion3").GetComponent<Text>();
         Opcion4 = GameObject.Find("Opcion4").GetComponent<Text>();
 
+      
+
         IniPosOp1 = new Vector3(-329.4f, 212.86f);
         IniPosOp2 = new Vector3(-331, 131);
         IniPosOp3 = new Vector3(-331.4f, 40.5f);
+        IniPosOp4 = new Vector3(-334f, -21f);
         flecha = GameObject.Find("Flecha").GetComponent<Text>();
 
         flecha.rectTransform.localPosition = new Vector2(7.600001f, 151.86f);
@@ -59,15 +62,22 @@ public class Menu : MonoBehaviour
         Opcion2.enabled = true;
 
 
-        Opcion3.text = ("SALIR");
+        Opcion3.text = ("COMO JUGAR?");
         Opcion3.transform.SetParent(lista.transform);
         Opcion3.transform.localPosition = IniPosOp3;
         Opcion3.enabled = true;
 
+        Opcion4.text = ("SALIR");
+        Opcion4.transform.SetParent(lista.transform);
+        Opcion4.transform.localPosition = IniPosOp4;
+        Opcion4.enabled = true;
 
 
-        Opcion4.enabled = false;
-        Opcion4.transform.SetParent(null);
+        TutorialImage = GameObject.Find("TutoImage").GetComponent<Image>();
+        TutorialImage.enabled = false;
+
+        // Opcion4.enabled = false;
+        //  Opcion4.transform.SetParent(null);
 
         title = GameObject.Find("Title").GetComponent<Text>();
         title.enabled = false;
@@ -81,18 +91,27 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool up = Input.GetKeyDown("up");
-        bool down = Input.GetKeyDown("down");
+        if (flecha.enabled)
+        {
+            bool up = Input.GetKeyDown("up");
+            bool down = Input.GetKeyDown("down");
 
-        if (up) indice--;
-        if (down) indice++;
+            if (up) indice--;
+            if (down) indice++;
 
-        if (indice > lista.transform.childCount - 1) indice = 0;
-        if (indice < 0) indice = lista.transform.childCount - 1;
+            if (indice > lista.transform.childCount - 1) indice = 0;
+            if (indice < 0) indice = lista.transform.childCount - 1;
 
-        if (up || down) Dibujar();
+            if (up || down) Dibujar();
 
-        if (Input.GetKeyDown("return") || Input.GetKeyDown("space") || Input.GetKeyDown("x")) Accion();
+            if (Input.GetKeyDown("return") || Input.GetKeyDown("space") || Input.GetKeyDown("x")) Accion();
+        }
+        
+        if (Input.GetKeyDown("c") && TutorialImage.isActiveAndEnabled)
+        {
+            TutorialImage.enabled = false;
+            flecha.enabled = true;
+        }
 
     }
 
@@ -105,7 +124,7 @@ public class Menu : MonoBehaviour
     void Accion()
     {
         Transform opcion = lista.transform.GetChild(indice);
-        if (opcion.gameObject.name == "Opcion3" && opcion.gameObject.GetComponent<Text>().text == "SALIR")
+        if (opcion.gameObject.name == "Opcion4" && opcion.gameObject.GetComponent<Text>().text == "SALIR")
         {
             print("cerrando Juego");
             Application.Quit();
@@ -122,9 +141,10 @@ public class Menu : MonoBehaviour
             Opcion1.text = ("2 Jugadores");
             Opcion2.text = ("3 Jugadores");
             Opcion3.text = ("4 Jugadores");
+            Opcion4.text = ("ATRAS");
 
         }
-        else if (opcion.gameObject.name == "Opcion4" || (opcion.gameObject.name == "Opcion1" && opcion.gameObject.GetComponent<Text>().text == "SALIR")) 
+        else if (opcion.gameObject.GetComponent<Text>().text == "ATRAS") 
         {
             //Para volver a Main Menu
             Start();
@@ -162,13 +182,19 @@ public class Menu : MonoBehaviour
            Opcion2.enabled = false;
             Opcion2.transform.SetParent(null);
 
-            Opcion1.text = ("SALIR");
+            Opcion1.text = ("ATRAS");
+           
             Opcion1.rectTransform.localPosition = new Vector2(-479, -243f);
-            flecha.rectTransform.localPosition = new Vector2(-119f, -322f);
+           flecha.rectTransform.localPosition = new Vector2(-119f, -322f);
+            
             Dibujar();
 
 
             SceneManager.LoadScene("NuevoJuego");
+        }         else if (opcion.gameObject.name == "Opcion3" && opcion.gameObject.GetComponent<Text>().text == "COMO JUGAR?")
+        {
+            TutorialImage.enabled = true;
+            flecha.enabled = false;
         }        
     }
 }
